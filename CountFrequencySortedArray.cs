529c21f1c18be620c0cf56515a2616b2fde7c215
Selim Gezer <27selimgezer@gmail.com>
2021-09-29 18:47:39 +0300

    Sorular

---
+++ CountFrequencySortedArray.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoruCozumleri
+{
+    class Program
+    {
+
+        //Aranan değerin dizide ilk karşılaştığımız indisini döndürür.
+        private static int binarySearch_ilkiBul(int[] dizi, int aranan)
+        {
+            int pointer1 = 0;
+            int pointer2 = dizi.Length - 1;
+
+            int ilkPozisyon = -1;
+
+            while (pointer1 <= pointer2)
+            {
+                int mid = (pointer1 + pointer2) / 2;
+
+                if (aranan == dizi[mid])
+                {
+                    ilkPozisyon = mid;
+
+                    pointer2 = mid - 1;
+                }
+                else if (aranan < dizi[mid])
+                {
+                    pointer2 = mid - 1;
+                }
+                else
+                {
+                    pointer1 = mid + 1;
+                }
+            }
+
+            return ilkPozisyon;
+        }
+
+        //Aranan değerin dizide son karşılaştığımız indisini döndürür.
+        private static int binarySearch_sonuBul(int[] dizi, int aranan)
+        {
+            int pointer1 = 0;
+            int pointer2 = dizi.Length - 1;
+
+            int sonPozisyon = -1;
+
+            while (pointer1 <= pointer2)
+            {
+                int mid = (pointer1 + pointer2) / 2;
+
+                if (aranan == dizi[mid])
+                {
+                    sonPozisyon = mid;
+
+                    pointer1 = mid + 1;
+                }
+                else if (aranan < dizi[mid])
+                {
+                    pointer2 = mid - 1;
+                }
+                else
+                {
+                    pointer1 = mid + 1;
+                }
+            }
+
+            return sonPozisyon;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] dizi = { 1,1,2,2,2,2,3 };
+            int aranan = 3;
+
+            int ilkIndex = binarySearch_ilkiBul(dizi,aranan);
+            int sonIndex = binarySearch_sonuBul(dizi,aranan);
+
+            int frekans = sonIndex - ilkIndex + 1;
+
+            Console.WriteLine(aranan + " degerinin frekansı:" + frekans);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SearchInInfiniteArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoruCozumleri
+{
+    class Program
+    {
+        
+        private static int binarySearch_Uyarlama(int[] dizi, int aranan)
+        {
+            int pointer1 = 0;
+            int pointer2 = 1;
+
+            while (aranan > dizi[pointer2])
+            {
+                pointer1 = pointer2;
+                pointer2 *= 2;
+            }
+
+            while (pointer1 <= pointer2)
+            {
+                int mid = (pointer1 + pointer2) / 2;
+
+                if (aranan == dizi[mid])
+                {
+                    return mid;
+                }
+                else if (aranan < dizi[mid])
+                {
+                    pointer2 = mid - 1;
+                }
+                else
+                {
+                    pointer1 = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] dizi = { 5,7,9,10,12,15,16 };
+            int aranan = 15;
+
+            int Index = binarySearch_Uyarlama(dizi,aranan);
+   
+            Console.WriteLine(aranan + " degerinin indexi:" + Index);
+
+            Console.ReadLine();
+        }
+    }
+}
